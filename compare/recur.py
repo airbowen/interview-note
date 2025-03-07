5763d3045a15623bfff10f64f2b77f68b6107abe
airbowen <airbowen@163.com>
2025-03-07 14:14:37 -0800

    compare  recursion

--- compare/recur.py
+++ compare/recur.py
@@ -1,5 +1,11 @@
 #xyz，要求返还xyz 所有的排列，比如，x，xy，xyz，xz，利用到recursion 的解法
 
+# 这个写法 不需要使用 pop()，因为 current 变量是 字符串（immutable，不可变对象），每次递归调用时，Python 会创建一个新的字符串实例，而不会修改原来的 current。
+
+# 如果是 列表（list），比如 current = []，那就需要 pop()，因为列表是 可变对象，递归回溯时要手动撤销状态，避免影响其他分支。
+
+# current + s[index] 生成了 一个新的字符串，并没有修改 current 本身，所以不会影响其他递归分支。
+
 def generate_subsequences(s, index=0, current="", result=None):
     if result is None:
         result = []
